cdb71416cce1197ecec426ba5763dbc3a758af19
bence1971387 <bence1971387@gmail.com>
2021-11-18 23:48:33 +0100

    ext4 test

--- 0-preinstall.sh
+++ 0-preinstall.sh
@@ -34,7 +34,7 @@ mkdir /mnt
 
 
 echo -e "\nInstalling prereqs...\n$HR"
-pacman -S --noconfirm gptfdisk btrfs-progs
+pacman -S --noconfirm gptfdisk
 
 echo "-------------------------------------------------"
 echo "-------select your disk to format----------------"
@@ -67,18 +67,18 @@ fi
 echo -e "\nCreating Filesystems...\n$HR"
 if [[ ${DISK} =~ "nvme" ]]; then
 mkfs.vfat -F32 -n "EFIBOOT" "${DISK}p2"
-mkfs.btrfs -L "ROOT" "${DISK}p3" -f
-mount -t btrfs "${DISK}p3" /mnt
+mkfs.ext4 -L "ROOT" "${DISK}p3" -f
+mount -t ext4 "${DISK}p3" /mnt
 else
 mkfs.vfat -F32 -n "EFIBOOT" "${DISK}2"
-mkfs.btrfs -L "ROOT" "${DISK}3" -f
-mount -t btrfs "${DISK}3" /mnt
-fi
-ls /mnt | xargs btrfs subvolume delete
-btrfs subvolume create /mnt/@
-umount /mnt
-;;
-*)
+mkfs.ext4 -L "ROOT" "${DISK}3" -f
+mount -t ext4 "${DISK}3" /mnt
+#fi
+#ls /mnt | xargs btrfs subvolume delete
+#btrfs subvolume create /mnt/@
+#umount /mnt
+#;;
+#*)
 echo "Rebooting in 3 Seconds ..." && sleep 1
 echo "Rebooting in 2 Seconds ..." && sleep 1
 echo "Rebooting in 1 Second ..." && sleep 1
@@ -87,7 +87,7 @@ reboot now
 esac
 
 # mount target
-mount -t btrfs -o subvol=@ -L ROOT /mnt
+mount -t ext4 -L ROOT /mnt
 mkdir /mnt/boot
 mkdir /mnt/boot/efi
 mount -t vfat -L EFIBOOT /mnt/boot/
